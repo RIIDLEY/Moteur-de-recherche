a3d611288a45b8e9590f85932d032bedb2ff29a8
RIIDLEY <hamsek.lahoucine@gmail.com>
2021-12-14 14:03:35 +0100

    css

--- Views/view_home.php
+++ Views/view_home.php
@@ -1,14 +1,14 @@
 <?php
 require('view_begin.php');
 ?>
-
-<form class="form-inline" action = "?controller=home&action=recherche" method="post" >
+<h1>Gogle</h1>
+<form class="form-inline" action = "?controller=home&action=recherche" method="post" style="display: inline-block;">
 				<input type="text" name="name" size="50" placeholder="Mot clés"/>
 				<input type="submit" value="Chercher" class="btn btn-primary mb-2"/></form>
 
 <ul>			
 <?php foreach ($liste as $key => $value):  ?>
-				<li>Mot trouvé dans le document : <a href="Content/doc/<?=$value["Document"]?>"><?=$value["Document"]?></a>. Avec <?=$value["Occurence"]?> occurence</li>
+				<li class="list-group-item">Mot trouvé dans le document : <a href="Content/doc/<?=$value["Document"]?>"><?=$value["Document"]?></a>. Avec <?=$value["Occurence"]?> occurence</li>
 <?php endforeach ?>
 </ul>	
 <?php
